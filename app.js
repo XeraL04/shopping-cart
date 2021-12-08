e9e328e310f664144adcdebc6fcf23fa94e50f78
Xeral04 <faresbelarbi@outlook.com>
2021-12-08 15:09:45 +0100

    first shot

---
+++ app.js
@@ -0,0 +1,127 @@
+//select el
+const productsEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items")
+const subtotalEl = document.querySelector(".subtotal")
+const totalItemsInCartEl = document.querySelector(".total-items-in-cart")
+
+//fun to rend prod
+function renderProducts(){
+    products.forEach((product)=>{
+        productsEl.innerHTML+=`
+        <div class="item">
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src="${product.imgSrc}" alt="${product.name}">
+                    </div>
+                    <div class="desc">
+                        <h2>${product.price}</h2>
+                        <h2><small>$</small>29.99</h2>
+                        <p>
+                            ${product.description}
+                        </p>
+                    </div>
+                    <div class="add-to-wishlist">
+                        <img src="./icons/heart.png" alt="add to wish list">
+                    </div>
+                    <div class="add-to-cart" onclick="addToCart(${product.id})">
+                        <img src="./icons/bag-plus.png" alt="add to cart">
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}
+renderProducts();
+//cart array 
+
+let cart=[];
+
+//add to cart
+function addToCart(id){
+    //check if product already exist in cart
+    if (cart.some((item) => item.id===id)) {
+        changeNumberOfUnits("plus", id)
+    }else{
+        const item = products.find((product) => product.id===id);
+        cart.push({
+            ...item,
+            numberOfUnits : 1,
+        });
+        
+    }
+updateCart();
+}
+
+//update cart 
+function updateCart(){
+    renderCartItems();
+    renderSubtotal();
+}
+
+//calculat and render sub
+
+function renderSubtotal(){
+    let totalPrice =0 , totalItems=0;
+
+    cart.forEach((item) =>{
+        totalPrice += item.price*item.numberOfUnits;
+        totalItems += item.numberOfUnits;
+    });
+
+    subtotalEl.innerHTML=`Subtotal (${totalItems} items): $${totalPrice.toFixed(2)}`
+    totalItemsInCartEl.innerHTML = totalItems;
+}
+
+//render cart items
+
+function renderCartItems(){
+    cartItemsEl.innerHTML=""; //clear cart elemnt
+    cart.forEach((item) => {
+        cartItemsEl.innerHTML +=`
+            <div class="cart-item">
+                <div class="item-info" onclick="removeItemFromCart(${item.id})">
+                    <img src="${item.imgSrc}" alt="${item.name}">
+                    <h4>${item.name}</h4>
+                </div>
+                <div class="unit-price">
+                    <small>$</small>${item.price}
+                </div>
+                <div class="units">
+                    <div class="btn minus onclick="changeNumberOfUnits('minus',${item.id} )">-</div>
+                    <div class="number">${item.numberOfUnits}</div>
+                    <div class="btn plus" onclick="changeNumberOfUnits('plus',${item.id} )">+</div>           
+                </div>
+            </div>
+        `
+    });
+}
+
+//remove item from cart
+function removeItemFromCart(id){
+    cart = cart.filter((item)=>item.id !==id );
+
+    updateCart();
+}
+
+// change number of units for a item 
+
+function changeNumberOfUnits(action , id){
+    cart = cart.map((item)=>{
+        
+        let numberOfUnits = item.numberOfUnits;
+
+        if(item.id===id){
+            if (action === "minus" && numberOfUnits > 1){
+                numberOfUnits--
+            }else if (action === "plus" && numberOfUnits < item.instock) {
+                numberOfUnits++
+            }
+        }
+        return {
+            ...item,
+            numberOfUnits,
+        };
+    });
+   
+    updateCart();
+}
